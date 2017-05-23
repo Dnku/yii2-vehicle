2aea7059422f645eddb6b3469233faafd64332ef
DNKU MEKONEN <dnkumk@gmail.com>
2017-05-23 17:43:12 +0300

    Update and rename AutoloadExample.php to AutoloadVehicle.php

--- AutoloadVehicle.php
+++ AutoloadVehicle.php
@@ -5,7 +5,7 @@ namespace mekonen;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class AutoloadVehicle extends \yii\base\Widget
 {
     public function run()
     {
